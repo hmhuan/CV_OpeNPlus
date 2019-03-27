785bebda1b98dc943d2104e8212228536b51356e
hmhuan <minhhuanhuynh289@gmail.com>
2019-03-27 16:45:36 +0700

    Canny

--- Lab01/1612855_1612858_BT02.py
+++ Lab01/1612855_1612858_BT02.py
@@ -2,19 +2,30 @@ import sys
 import cv2 as cv
 import numpy as np 
 from matplotlib import pyplot as plt
-from edge_detector import Sobel, Prewitt, Laplace
+from EdgeDetector import Sobel, Prewitt, Laplace, Canny
 
-def show(srcImage, dstImage, Gx, Gy, name):
-    plt.subplot(2,2,1),plt.imshow(srcImage,cmap = 'gray')
-    plt.title('Grayscale'), plt.xticks([]), plt.yticks([])
-    plt.subplot(2,2,2),plt.imshow(dstImage,cmap = 'gray')
-    plt.title(name), plt.xticks([]), plt.yticks([])
-    if Gx != None:
+def show(name, srcImage, dstImage, Gx = None, Gy = None):
+
+    if (name == "Sobel" or name == "Prewitt"):
+        #Show GrayImage
+        plt.subplot(2,2,1),plt.imshow(srcImage,cmap = 'gray')
+        plt.title('Grayscale'), plt.xticks([]), plt.yticks([])
+        #Show final Image
+        plt.subplot(2,2,2),plt.imshow(dstImage,cmap = 'gray')
+        plt.title(name), plt.xticks([]), plt.yticks([])
+        #Show Gx
         plt.subplot(2,2,3),plt.imshow(Gx,cmap = 'gray')
         plt.title(name + " X"), plt.xticks([]), plt.yticks([])
-    if Gy != None:
+        #Show Gy
         plt.subplot(2,2,4),plt.imshow(Gy,cmap = 'gray')
         plt.title(name + "Y"), plt.xticks([]), plt.yticks([])
+    else:
+        #Show GrayImage
+        plt.subplot(1,2,1),plt.imshow(srcImage,cmap = 'gray')
+        plt.title('Grayscale'), plt.xticks([]), plt.yticks([])
+        #Show final Image
+        plt.subplot(1,2,2),plt.imshow(dstImage,cmap = 'gray')
+        plt.title(name), plt.xticks([]), plt.yticks([])
     plt.show()
 
 
@@ -25,10 +36,13 @@ if __name__ == "__main__":
 
     if operator == "--sobel":
         Gx, Gy, dstImage = Sobel(srcImage)
-        show(srcImage, dstImage, Gx, Gy, "Sobel")
+        show( "Sobel", srcImage, dstImage, Gx, Gy)
     elif operator == "--prewitt":
         Gx, Gy, dstImage = Sobel(srcImage)
-        show(srcImage, dstImage, Gx, Gy, "Prewitt")
+        show("Prewitt", srcImage, dstImage, Gx, Gy)
     elif operator == "--laplace":
         dstImage = Laplace(srcImage)
-        show(srcImage, dstImage, Gx=None, Gy=None, name="Laplace")
+        show("Laplace", srcImage, dstImage)
+    elif operator == "--canny":
+        dstImage = Canny(srcImage)
+        show("Canny", srcImage, dstImage)
--- Lab01/EdgeDetector.py
+++ Lab01/EdgeDetector.py
@@ -3,11 +3,11 @@ import numpy as np
 
 def saturate_cast(val):
     """ Fix value of a pixel to saturate with image type """
-    if val < 0:
-        val = 0
-    if val > 255:
-        val = 255
-    return val
+    if int(val) < 0:
+        return 0
+    if int(val) > 255:
+        return 255
+    return int(val)
 
 
 def Sobel(srcImage):
@@ -19,23 +19,26 @@ def Sobel(srcImage):
     # Get height and width of @srcImage
     height, width = srcImage.shape
     # Create @dstImage with shape and type of @srcImage
-    Gx = np.zeros((height, width), srcImage.dtype)
-    Gy = np.zeros((height, width), srcImage.dtype)
-    dstImage = np.zeros((height, width), srcImage.dtype)
+    Gx = np.zeros((height, width))
+    Gy = np.zeros((height, width))
+    dstImage = np.zeros((height, width))
     # Kernel of the derivative in x direction    
-    Wx = np.array([[0.25, 0, -0.25], [0.5, 0, -0.5], [0.25, 0, -0.25]])
+    #Wx = np.array([[0.25, 0, -0.25], [0.5, 0, -0.5], [0.25, 0, -0.25]])
+    Wx = np.array([[0.5], [1],[0.5]])
+    Wy = np.array([[-0.5], [0], [0.5]])
     # Kernel of the derivative in y direction    
-    Wy = np.array([[-0.25, -0.5, -0.25], [0, 0, 0], [0.25, 0.5, 0.25]])
+    #Wy = np.array([[-0.25, -0.5, -0.25], [0, 0, 0], [0.25, 0.5, 0.25]])
     for row in range(1, height-1):
         for col in range(1, width-1):
             # The derivative in x direction of pixel[rol, col]
-            fx = (srcImage[row-1:row+2, col-1:col+2] * Wx).sum()
+            temp = srcImage[row - 1 : row + 2, col - 1 : col + 2]
+            fx = np.dot(Wx.T, np.dot(temp,Wy)).reshape(1)
             Gx[row, col] = saturate_cast(fx)
             # The derivative in y direction of pixel[rol, col]
-            fy = (srcImage[row-1:row+2, col-1:col+2] * Wy).sum()
+            fy = np.dot(Wy.T, np.dot(temp, Wx)).reshape(1)
             Gy[row, col] = saturate_cast(fy)
             # Approximation of the gradient in that pixel
-            dstImage[row, col] = saturate_cast(fx + fy)
+            dstImage[row, col] = saturate_cast(abs(fx + fy))
     return Gx, Gy, dstImage
 
     
@@ -55,14 +58,15 @@ def Prewitt(srcImage, dstImage):
     Wy = np.array([[-1/3, -1/3, -1/3], [0, 0, 0], [1/3, 1/3, 1/3]])
     for row in range(1, height-1):
         for col in range(1, width-1):
+            temp = srcImage[row - 1 : row + 2, col - 1 : col + 2]
             # The derivative in x direction of pixel[rol, col]
-            fx = (srcImage[row-1:row+2, col-1:col+2] * Wx).sum()
+            fx = (temp * Wx).sum()
             Gx[row, col] = saturate_cast(fx)
             # The derivative in y direction of pixel[rol, col]
-            fy = (srcImage[row-1:row+2, col-1:col+2] * Wy).sum()
+            fy = (temp * Wy).sum()
             Gy[row, col] = saturate_cast(fy)
             # Approximation of the gradient in that pixel
-            dstImage[row, col] = saturate_cast(fx + fy)
+            dstImage[row, col] = saturate_cast(abs(fx + fy))
     return Gx, Gy, dstImage
 
 
@@ -82,4 +86,76 @@ def Laplace(srcImage):
         for col in range(1, width-1):
             val = (srcImage[row-1:row+2, col-1:col+2] * WLap).sum()
             dstImage[row, col] = saturate_cast(val)
+    return dstImage
+
+
+def Hysteresis(gradNms, dstImage, x, y, low):
+    
+    if (x < 0 or y < 0 or x >= dstImage.shape[0] or y >= dstImage.shape[1] or dstImage[x, y] != 0):
+        return 
+    if (gradNms[x, y] < low):
+        return
+    dstImage[x, y] = 255
+    
+    Hysteresis(gradNms, dstImage, x - 1, y - 1, low)
+    Hysteresis(gradNms, dstImage, x - 1, y, low)
+    Hysteresis(gradNms, dstImage, x - 1, y + 1, low)
+    Hysteresis(gradNms, dstImage, x , y - 1, low)
+    Hysteresis(gradNms, dstImage, x , y + 1, low)
+    Hysteresis(gradNms, dstImage, x + 1, y - 1, low)
+    Hysteresis(gradNms, dstImage, x + 1, y, low)
+    Hysteresis(gradNms, dstImage, x + 1, y + 1, low)
+
+def Canny(srcImage):
+
+    (m, n) = srcImage.shape
+    lowThreshold = 30
+    highThreshold = lowThreshold * 3
+    # Initial Gradient X, Gradient Y and Edge
+    Gx = np.zeros((m, n), dtype = srcImage.dtype)
+    Gy = np.zeros((m, n), dtype = srcImage.dtype)
+    G = np.zeros((m, n), dtype = srcImage.dtype)
+    dstImage = np.zeros((m, n), dtype = srcImage.dtype)
+
+    # Step 1: Gaussian filter
+    blurImg = cv.GaussianBlur(srcImage, (5, 5), 0)
+
+    # Step 2: Find Orientation at each pixel and Non-maximum suppression
+    # Mat Wx, Wy
+    Wx = np.array([[1], [2],[1]])
+    Wy = np.array([[-1], [0], [1]])
+    for i in range(1, m - 1):
+        for j in range(1, n - 1):
+            #Slice matrix
+            temp = srcImage[i - 1 : i + 2, j - 1 : j + 2]
+            #calc Gx(i, j), Gy(i, j)
+            fx = np.dot(np.dot(Wy.T, temp), Wx).reshape(1) #(Wx * temp).sum()
+            Gx[i, j] = saturate_cast(fx)
+            fy = np.dot(np.dot(Wx.T, temp), Wy).reshape(1) #(Wy * temp).sum()
+            Gy[i, j] = saturate_cast(fy)
+            G[i, j] = saturate_cast(abs(fx + fy))
+
+    gradNms = G.copy()
+    Theta = np.fmod(np.arctan2(Gy, Gx) + np.pi, np.pi) / np.pi * 4
+    #Non-maximum Supression
+    for i in range(1, m - 1):
+        for j in range(1, n - 1):
+            Ga, Gb = 0, 0
+            if (Theta[i, j] == 0):
+                Ga, Gb = G[i, j -1], G[i, j + 1]
+            elif (Theta[i, j] == 1):
+                Ga, Gb = G[i - 1, j + 1], G[i + 1, j - 1]
+            elif (Theta[i, j] == 2):
+                Ga, Gb = G[i - 1, j], G[i + 1, j]
+            else:
+                Ga, Gb = G[i - 1, j - 1], G[i + 1, j + 1]
+            if (G[i, j] < Ga) or (G[i, j] < Gb):
+                gradNms[i, j] = 0
+
+    # Step 3: Hysteresis Thresholding  [L, H] Recursive
+    for i in range(1, m - 1):
+        for j in range(1, n - 1):
+            if (gradNms[i, j] >= highThreshold):
+                Hysteresis(gradNms, dstImage, i, j, lowThreshold)
+    
     return dstImage
