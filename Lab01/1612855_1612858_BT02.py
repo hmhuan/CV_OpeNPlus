be4b9090f87140e8a4bac59e7921bf50ddd6100c
hmhuan <minhhuanhuynh289@gmail.com>
2019-03-30 16:21:16 +0700

    Update Canny Detector Edge

--- Lab01/1612855_1612858_BT02.py
+++ Lab01/1612855_1612858_BT02.py
@@ -5,34 +5,18 @@ from matplotlib import pyplot as plt
 from EdgeDetector import Sobel, Prewitt, Laplace, Canny
 
 def show(name, srcImage, dstImage, Gx = None, Gy = None):
-
-    if (name == "Sobel" or name == "Prewitt"):
-        #Show GrayImage
-        plt.subplot(2,2,1),plt.imshow(srcImage,cmap = 'gray')
-        plt.title('Grayscale'), plt.xticks([]), plt.yticks([])
-        #Show final Image
-        plt.subplot(2,2,2),plt.imshow(dstImage,cmap = 'gray')
-        plt.title(name), plt.xticks([]), plt.yticks([])
-        #Show Gx
-        plt.subplot(2,2,3),plt.imshow(Gx,cmap = 'gray')
-        plt.title(name + " X"), plt.xticks([]), plt.yticks([])
-        #Show Gy
-        plt.subplot(2,2,4),plt.imshow(Gy,cmap = 'gray')
-        plt.title(name + "Y"), plt.xticks([]), plt.yticks([])
-    else:
-        #Show GrayImage
-        plt.subplot(1,2,1),plt.imshow(srcImage,cmap = 'gray')
-        plt.title('Grayscale'), plt.xticks([]), plt.yticks([])
-        #Show final Image
-        plt.subplot(1,2,2),plt.imshow(dstImage,cmap = 'gray')
-        plt.title(name), plt.xticks([]), plt.yticks([])
-    plt.show()
+    if (name == "Prewitt" or name == "Sobel"):
+        cv.imshow("Gradient by x", Gx)
+        cv.imshow("Gradient by y", Gy)
+    cv.imshow(name, dstImage)
 
 
 if __name__ == "__main__":
     filename = sys.argv[1]
     operator = sys.argv[2]
     srcImage = cv.imread(filename, cv.IMREAD_GRAYSCALE)
+
+    cv.imshow("Source image", srcImage)
 
     if operator == "--sobel":
         Gx, Gy, dstImage = Sobel(srcImage)
@@ -44,5 +28,10 @@ if __name__ == "__main__":
         dstImage = Laplace(srcImage)
         show("Laplace", srcImage, dstImage)
     elif operator == "--canny":
-        dstImage = Canny(srcImage)
+        dstImage, low, high = Canny(srcImage)
         show("Canny", srcImage, dstImage)
+        blurImg = cv.GaussianBlur(srcImage, (5, 5), 1.4)
+        CannyByOpenCV = cv.Canny(blurImg, low, high)
+        cv.imshow("Canny by OpenCV", CannyByOpenCV)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
--- Lab01/EdgeDetector.py
+++ Lab01/EdgeDetector.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import numpy as np 
+import math
 
+# Hàm ép kiểu và kiểu uint8
 def saturate_cast(val):
     """ Fix value of a pixel to saturate with image type """
     if int(val) < 0:
@@ -9,7 +11,9 @@ def saturate_cast(val):
         return 255
     return int(val)
 
-
+# Hàm phát hiện biên cạnh bằng toán tử Sobel
+# Input: ảnh nguồn
+# Output: ảnh đạo hàm theo x, y và ảnh đích
 def Sobel(srcImage):
     """ Edge detector using Sobel operator
         Argument:
@@ -19,9 +23,9 @@ def Sobel(srcImage):
     # Get height and width of @srcImage
     height, width = srcImage.shape
     # Create @dstImage with shape and type of @srcImage
-    Gx = np.zeros((height, width))
-    Gy = np.zeros((height, width))
-    dstImage = np.zeros((height, width))
+    Gx = np.zeros((height, width), dtype = srcImage.dtype)
+    Gy = np.zeros((height, width), dtype = srcImage.dtype)
+    dstImage = np.zeros((height, width), dtype = srcImage.dtype)
     # Kernel of the derivative in x direction    
     Wx = np.array([[0.25, 0, -0.25], [0.5, 0, -0.5], [0.25, 0, -0.25]])
     # Kernel of the derivative in y direction    
@@ -40,7 +44,9 @@ def Sobel(srcImage):
             dstImage[row, col] = saturate_cast(abs(fx + fy))
     return Gx, Gy, dstImage
 
-    
+# Hàm phát hiện biên cạnh bằng toán tử Prewitt
+# Input: ảnh nguồn
+# Output: ảnh đạo hàm theo x, y và ảnh đích
 def Prewitt(srcImage, dstImage):
     """ Edge detector using Sobel operator
         Argument:
@@ -50,11 +56,13 @@ def Prewitt(srcImage, dstImage):
     # Get height and width of @srcImage
     height, width = srcImage.shape
     # Create @dstImage with shape and type of @srcImage
-    Gx, Gy, dstImage = np.zeros((height, width), srcImage.dtype)
+    Gx = np.zeros((height, width), dtype = srcImage.dtype)
+    Gy = np.zeros((height, width), dtype = srcImage.dtype)
+    dstImage = np.zeros((height, width), dtype = srcImage.dtype)
     # Kernel of the derivative in x direction  
-    Wx = np.array([[1/3, 0, -1/3], [1/3, 0, -1/3], [1/3, 0, -1/3]])
+    Wx = np.array([[-1/3, 0, 1/3], [-1/3, 0, 1/3], [-1/3, 0, 1/3]])
     # Kernel of the derivative in y direction  
-    Wy = np.array([[-1/3, -1/3, -1/3], [0, 0, 0], [1/3, 1/3, 1/3]])
+    Wy = np.array([[1/3, 1/3, 1/3], [0, 0, 0], [-1/3, -1/3, -1/3]])
     for row in range(1, height-1):
         for col in range(1, width-1):
             temp = srcImage[row - 1 : row + 2, col - 1 : col + 2]
@@ -68,7 +76,9 @@ def Prewitt(srcImage, dstImage):
             dstImage[row, col] = saturate_cast(abs(fx + fy))
     return Gx, Gy, dstImage
 
-
+# Hàm phát hiện biên cạnh bằng toán tử Laplace
+# Input: ảnh nguồn
+# Output: ảnh đích
 def Laplace(srcImage):
     """ Edge detector using Laplace operator
         Argument:
@@ -78,7 +88,7 @@ def Laplace(srcImage):
     # Get height and width of @srcImage
     height, width = srcImage.shape
     # Create @dstImage with shape and type of @srcImage
-    dstImage = np.zeros((height, width), srcImage.dtype)
+    dstImage = np.zeros((height, width), dtype = srcImage.dtype)
     # Lapalce kernel
     WLap = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
     for row in range(1, height-1):
@@ -87,74 +97,84 @@ def Laplace(srcImage):
             dstImage[row, col] = saturate_cast(val)
     return dstImage
 
-
-def Hysteresis(gradNms, dstImage, x, y, low):
-    
-    if (x < 0 or y < 0 or x >= dstImage.shape[0] or y >= dstImage.shape[1] or dstImage[x, y] != 0):
-        return 
-    if (gradNms[x, y] < low):
-        return
-    dstImage[x, y] = 255
-    
-    Hysteresis(gradNms, dstImage, x - 1, y - 1, low)
-    Hysteresis(gradNms, dstImage, x - 1, y, low)
-    Hysteresis(gradNms, dstImage, x - 1, y + 1, low)
-    Hysteresis(gradNms, dstImage, x , y - 1, low)
-    Hysteresis(gradNms, dstImage, x , y + 1, low)
-    Hysteresis(gradNms, dstImage, x + 1, y - 1, low)
-    Hysteresis(gradNms, dstImage, x + 1, y, low)
-    Hysteresis(gradNms, dstImage, x + 1, y + 1, low)
-
-def Canny(srcImage):
+# Hàm Canny detector
+# Input: ảnh nguồn (ảnh xám), lowThreshold, highTheshold
+# Output: ảnh đích
+def Canny(srcImage, lowThreshold = 0, highThreshold = 0):
 
     (m, n) = srcImage.shape
-    lowThreshold = 30
-    highThreshold = lowThreshold * 3
+    if (lowThreshold == 0):
+        highThreshold = 30
+        lowThreshold = 90
+    
     # Initial Gradient X, Gradient Y and Edge
-    Gx = np.zeros((m, n), dtype = srcImage.dtype)
-    Gy = np.zeros((m, n), dtype = srcImage.dtype)
+    Theta = np.zeros((m, n))
     G = np.zeros((m, n), dtype = srcImage.dtype)
-    dstImage = np.zeros((m, n), dtype = srcImage.dtype)
+    
+    # Step 1: Preprocessing - Gaussian filter
+    blurImg = cv.GaussianBlur(srcImage, (5, 5), 1.4)
 
-    # Step 1: Gaussian filter
-    blurImg = cv.GaussianBlur(srcImage, (5, 5), 0)
-
-    # Step 2: Find Orientation at each pixel and Non-maximum suppression
-    # Mat Wx, Wy
-    Wx = np.array([[1], [2],[1]])
-    Wy = np.array([[-1], [0], [1]])
+    # Step 2: Find Orientation and Magnitude at each pixel and Non-maximum suppression
+    # Mat Wx, Wy - 2 ma trận đạo hàm đã "ngược hóa" để thuận lới tính tích chập
+    Wx = np.array([[-1, 0, 1], [-2, 0, 2],[-1, 0, 1]])
+    Wy = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
     for i in range(1, m - 1):
         for j in range(1, n - 1):
-            #Slice matrix
-            temp = srcImage[i - 1 : i + 2, j - 1 : j + 2]
-            #calc Gx(i, j), Gy(i, j)
-            fx = np.dot(np.dot(Wy.T, temp), Wx).reshape(1) #(Wx * temp).sum()
-            Gx[i, j] = saturate_cast(fx)
-            fy = np.dot(np.dot(Wx.T, temp), Wy).reshape(1) #(Wy * temp).sum()
-            Gy[i, j] = saturate_cast(fy)
-            G[i, j] = saturate_cast(abs(fx + fy))
-
-    gradNms = G.copy()
-    Theta = np.fmod(np.arctan2(Gy, Gx) + np.pi, np.pi) / np.pi * 4
+            #Slice matrix - cắt kaays ma trận 3x3 để tính toán
+            temp = blurImg[i - 1 : i + 2, j - 1 : j + 2]
+            #Tính G, Theta
+            # Nhân chập - bản chất là nhân với ma trận đã "ngược hóa" sau đó lấy tổng
+            fx = (Wx * temp).sum()
+            fy = (Wy * temp).sum()
+            # G = (fx^2 + fy^2)^(1/2)
+            G[i, j] = saturate_cast(math.sqrt(fx*fx + fy*fy))
+            # Quy đổi về góc 0 -> 180 để dễ xét bin
+            Theta[i, j] = np.arctan2(fy, fx) * 180 / math.pi
+            if Theta[i, j] < 0: 
+                Theta[i, j] += 180
+    
+    gradNms = np.zeros((m, n), dtype = G.dtype)
     #Non-maximum Supression
     for i in range(1, m - 1):
         for j in range(1, n - 1):
-            Ga, Gb = 0, 0
-            if (Theta[i, j] == 0):
-                Ga, Gb = G[i, j -1], G[i, j + 1]
-            elif (Theta[i, j] == 1):
-                Ga, Gb = G[i - 1, j + 1], G[i + 1, j - 1]
-            elif (Theta[i, j] == 2):
+            # Chia làm 4 bin với 2 nửa bin là 0 -> 22.5 và 157.5 -> 180 => bin 1
+            if (Theta[i, j] < 22.5 or Theta[i, j] >= 157.5):
+                Ga, Gb = G[i, j - 1], G[i, j + 1]
+            # bin 2: 22.5 -> 67.5
+            elif (Theta[i, j] >= 22.5 and Theta[i, j] < 67.5):
+                Ga, Gb = G[i - 1, j - 1], G[i + 1, j + 1]
+            # bin 3: 67.5 -> 112.5
+            elif (Theta[i, j] >= 67.5 and Theta[i, j] < 112.5):
                 Ga, Gb = G[i - 1, j], G[i + 1, j]
             else:
-                Ga, Gb = G[i - 1, j - 1], G[i + 1, j + 1]
-            if (G[i, j] < Ga) or (G[i, j] < Gb):
-                gradNms[i, j] = 0
-
+                #bin 4: từ 112.5 -> 157.5
+                Ga, Gb = G[i + 1, j - 1], G[i - 1, j + 1]
+            # Các điểm kề mà nhỏ hơn điểm đang xét thì không loại bỏ nó (ở đây ban đầu gán là 0 nên gán nó là giá trị cường độ gradient tại i, j)
+            if (G[i, j] >= Ga) and (G[i, j] >= Gb):
+                gradNms[i, j] = G[i, j]
+    
     # Step 3: Hysteresis Thresholding  [L, H] Recursive
+    #Khởi tạo ảnh đích với các giá trị lớn hơn ngưỡng trên là 255, ngược lại là 0
+    dstImage = np.array(gradNms >= highThreshold, dtype = srcImage.dtype) * 255
+    #Danh sách lưu trữ các điểm weak
+    currentPixels = []
     for i in range(1, m - 1):
         for j in range(1, n - 1):
-            if (gradNms[i, j] >= highThreshold):
-                Hysteresis(gradNms, dstImage, i, j, lowThreshold)
-    
-    return dstImage
+            if (gradNms[i, j] >= lowThreshold and dstImage[i, j] == 0):
+                # Những điểm nằm giữa ngưỡng thấp và ngưỡng cao (weak)
+                localMax = gradNms[i - 1: i + 2, j - 1: j + 2].max()
+                if (localMax >= highThreshold):
+                    currentPixels.append((i, j))
+                    dstImage[i, j] = 255
+                
+    # Duyệt và tìm các điểm weak kề các điểm strong
+    while len(currentPixels) > 0:
+        currentPixel = currentPixels.pop()
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if (i != 0  and j != 0):
+                    x, y = currentPixel[0] + i, currentPixel[1] + j
+                    if (x > 0 and x < gradNms.shape[0] and y > 0 and y < gradNms.shape[1] and gradNms[x, y] >= lowThreshold and dstImage[x, y] == 0):
+                        dstImage[x, y] = 255
+                        currentPixels.append((x, y))
+    return dstImage, lowThreshold, highThreshold
