6c9a3af318a5756532e8c8dbe0ea6d2d48ef0c1f
hmhuan <minhhuanhuynh289@gmail.com>
2019-04-22 23:01:51 +0700

    update code blob, hinh nhu khong dung. up file exe

--- Lab03/CV_Lab03/Source.cpp
+++ Lab03/CV_Lab03/Source.cpp
@@ -2,17 +2,16 @@
 
 int main(int argc, char * argv[])
 {
-	Mat srcImg = imread(argv[1], IMREAD_COLOR), GrayImg, dstImg; //auto load image with BGR
-	Mat compImg, compGrayImg;
-	Mat R;
+	Mat srcImg = imread(argv[1], IMREAD_COLOR), GrayImg, dstImg; //auto load image with BGR.
+	Mat compImg, compGrayImg; // Ảnh cần compare, và ảnh xám của nó.
+	Mat R, blob, DoG; // ma trận Respond, blob, DoG.
 	int code = atoi(argv[2]), kSize, blockSize, k, detector;
-	float Threshold, sigma, alpha;
-	double result;
+	float Threshold, sigma, alpha; // ngưỡng, giá trị sigma, giá trị alpha (trong Respond function).
+	double result; // Kết quả trả về khi áp dụng mathSIFT.
 	
 	// convert ảnh sang ảnh xám.
 	cvtColor(srcImg, GrayImg, COLOR_BGR2GRAY);
 	
-
 	switch (code) {
 	case 1: // grid.jpg 1 13 1000000000 0.05 | lena.png 1 7 1000000000.0 0.05
 		blockSize = atoi(argv[3]); //17;
@@ -21,8 +20,10 @@ int main(int argc, char * argv[])
 		R = DetectHarris(GrayImg, blockSize, 5, alpha, Threshold);
 		dstImg = ImageWithFeature(srcImg, R, Threshold);
 		break;
-	case 2:
-		dstImg = detectBlob(GrayImg);
+	case 2: // blob.jpg 2 5
+		kSize = atoi(argv[3]);
+		blob = detectBlob(GrayImg, kSize);
+		dstImg = ImageWithFeature(srcImg, blob, 0);
 		break;
 	case 3: //grid.jpg 3 5 1 5
 		kSize = atoi(argv[3]);
@@ -46,7 +47,6 @@ int main(int argc, char * argv[])
 	{
 		cout << "Successful.\n";
 		imshow("Image with Features", dstImg);
-		imwrite("res_10.jpg", dstImg);
 	}
 	else 
 	{
